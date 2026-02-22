c83eb46df742c392f4c4c958360cb14ea24d1f0b
Charles Chang <42170907+CharlesVoyager@users.noreply.github.com>
2026-02-22 09:56:42 +0800

    save

---
+++ view/wpf/UI_object_information.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View3D.view.wpf
+{
+    /// <summary>
+    /// Interaction logic for UI_object_information.xaml
+    /// </summary>
+    public partial class UI_object_information : UserControl
+    {
+        public UI_object_information()
+        {
+            InitializeComponent();
+        }
+    }
+}

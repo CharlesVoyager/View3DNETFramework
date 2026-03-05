14af5cd90b1b45d9a78bf71581726f523848ad43
Charles Chang <42170907+CharlesVoyager@users.noreply.github.com>
2026-03-05 15:00:14 +0800

    Improvement.

--- ModelLib/Utils/RamTools.cs
+++ ModelLib/Utils/RamTools.cs
@@ -8,8 +8,14 @@ namespace View3D.Utils
         public static bool IsRamSizeValid()
         {
             bool valid = true;
-            //ulong totalRam = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+#if NET10_0_OR_GREATER
+            var gcInfo = GC.GetGCMemoryInfo();
+            ulong totalRam = (ulong)gcInfo.TotalAvailableMemoryBytes / 1024 / 1024;  // MB
+            ulong committedRam = (ulong)gcInfo.TotalCommittedBytes / 1024 / 1024;  // MB
+            ulong availRam = totalRam - committedRam;
+#else   // .NET Framework 4.8
             ulong availRam = new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / 1024 / 1024;    // Unit: MB
+#endif
 
             if (availRam < SWSetting.Memory.RemainMin || getCurMemoryUsed() >= SWSetting.Memory.UsedLimit_64bit)
             {
